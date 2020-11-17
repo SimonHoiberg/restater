84d74cbbe520b95f9b24284dc594b8c3f5182bad
Simon LH <admin@silind.com>
2020-11-17 20:24:03 +0100

    Derive state from callbac

--- src/useAsyncStore.ts
+++ src/useAsyncStore.ts
@@ -3,19 +3,32 @@ import { useContext } from 'react';
 
 /**
  * Returns an async payload from a store, and a function to update it.
- * @param context 
- * @param key 
+ * @param context
+ * @param key
  */
 const useAsyncStore = <T, K extends keyof T>(context: React.Context<IAsyncContext<T>>, key: K) => {
   const { store, setStore } = useContext(context);
   const state = store[key];
 
-  const setState = async (promise: Promise<T[K]>) => {
+  const setState = async (
+    promiseOrCallback:
+      | Promise<T[K]>
+      | ((currentValue: { [K in keyof T]: AsyncPayload<T[K]> }[K]) => Promise<T[K]>),
+  ) => {
     setStore({ prop: key, payload: { state: 'loading' } });
 
     try {
-      const data = await promise;
-      setStore({ prop: key, payload: { state: 'completed', data } });
+      if (typeof promiseOrCallback === 'function') {
+        const callback = promiseOrCallback as (
+          currentValue: { [K in keyof T]: AsyncPayload<T[K]> }[K],
+        ) => Promise<T[K]>;
+        const data = await callback(state);
+        setStore({ prop: key, payload: { state: 'completed', data } });
+      } else {
+        const promise = promiseOrCallback as Promise<T[K]>;
+        const data = await promise;
+        setStore({ prop: key, payload: { state: 'completed', data } });
+      }
     } catch (error) {
       setStore({ prop: key, payload: { state: 'failed', error } });
     }
