f6060e4770c1f2dfe0d439ceba64761f9c3be418
Simon LH <admin@silind.com>
2020-11-18 06:27:26 +0100

    Keep outer-store references to avoid capturing pre-render values

--- src/useAsyncStore.ts
+++ src/useAsyncStore.ts
@@ -2,12 +2,19 @@ import { IAsyncContext, AsyncPayload } from './interfaces/interfaces';
 import { useContext } from 'react';
 
 /**
+ * Keep an outer-store reference to avoid capturing pre-render values
+ * in the setState function closure.
+ */
+const outerStore: any = {};
+
+/**
  * Returns an async payload from a store, and a function to update it.
  * @param context
  * @param key
  */
 const useAsyncStore = <T, K extends keyof T>(context: React.Context<IAsyncContext<T>>, key: K) => {
   const { store, setStore } = useContext(context);
+  outerStore[key] = store[key];
 
   const setState = async (
     promiseOrCallback:
@@ -21,7 +28,7 @@ const useAsyncStore = <T, K extends keyof T>(context: React.Context<IAsyncContex
         const callback = promiseOrCallback as (
           currentValue: { [K in keyof T]: AsyncPayload<T[K]> }[K],
         ) => Promise<T[K]>;
-        const data = await callback(store[key]);
+        const data = await callback(outerStore[key]);
         setStore({ prop: key, payload: { state: 'completed', data } });
       } else {
         const promise = promiseOrCallback as Promise<T[K]>;
--- src/useStore.ts
+++ src/useStore.ts
@@ -2,17 +2,24 @@ import { useContext } from 'react';
 import { IContext } from './interfaces/interfaces';
 
 /**
+ * Keep an outer-store reference to avoid capturing pre-render values
+ * in the setState function closure.
+ */
+const outerStore: any = {};
+
+/**
  * Returns a stateful value from a store, and a function to update it.
  * @param context
  * @param key
  */
 const useStore = <T, K extends keyof T>(context: React.Context<IContext<T>>, key: K) => {
   const { store, setStore } = useContext(context);
+  outerStore[key] = store[key];
 
   const setState = (valueOrCallback: T[K] | ((currentValue: T[K]) => T[K])) => {
     if (typeof valueOrCallback === 'function') {
       const callback = valueOrCallback as (currentValue: T[K]) => T[K];
-      setStore({ prop: key, payload: callback(store[key]) });
+      setStore({ prop: key, payload: callback(outerStore[key]) });
     } else {
       const value = valueOrCallback as T[K];
       setStore({ prop: key, payload: value });
